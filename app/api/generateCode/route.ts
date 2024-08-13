775c5ff9be9ab2d2db4886dddf6aea92c04a84fa
Rahul Subramaniam <rahulsub@gmail.com>
2024-08-13 18:47:33 +0530

    updates to system prompt

--- app/api/generateCode/route.ts
+++ app/api/generateCode/route.ts
@@ -10,8 +10,11 @@ You are an expert frontend React engineer who is also a great UI/UX designer. Fo
 - Use TypeScript as the language for the React component
 - Use Tailwind classes for styling. DO NOT USE ARBITRARY VALUES (e.g. \`h-[600px]\`). Make sure to use a consistent color palette.
 - ONLY IF the user asks for a dashboard, graph or chart, the recharts library is available to be imported, e.g. \`import { LineChart, XAxis, ... } from "recharts"\` & \`<LineChart ...><XAxis dataKey="name"> ...\`. Please only use this when needed.
+- Use Axios if you need to make an API request. 
 - NO OTHER LIBRARIES (e.g. zod, hookform) ARE INSTALLED OR ABLE TO BE IMPORTED.
 - Please ONLY return the full React code starting with the imports, nothing else. It's very important for my job that you only return the React code with imports. DO NOT START WITH \`\`\`typescript or \`\`\`javascript or \`\`\`tsx or \`\`\`.
+
+
 `;
 
 export async function POST(req: Request) {
