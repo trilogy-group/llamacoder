9ce678c6e2c12f098a991a41c7b08013c27c95d3
Rishabh Raizada <rishabh.raizada@trilogy.com>
2024-09-06 18:39:41 +0530

    removed deprecated config for rendering images

--- next.config.mjs
+++ next.config.mjs
@@ -4,7 +4,12 @@ const nextConfig = {
     serverComponentsExternalPackages: ["googleapis"],
   },
   images: {
-    domains: ["lh3.googleusercontent.com"],
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "lh3.googleusercontent.com",
+      },
+    ],
   },
 };
 
