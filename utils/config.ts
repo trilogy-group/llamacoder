262c66c7d30b52f903c1ebc8953259cddcf25d0e
amandal-cn <mandal.ashish@codenation.co.in>
2024-09-11 08:11:46 +0530

    separated static configs

---
+++ utils/config.ts
@@ -0,0 +1,51 @@
+export const defaultDependencies = [
+    {
+      name: "lucide-react",
+      version: "latest",
+    },
+    {
+      name: "recharts",
+      version: "latest",
+    },
+    {
+      name: "axios",
+      version: "latest",
+    },
+    {
+      name: "react-dom",
+      version: "latest",
+    },
+    {
+      name: "react-router-dom",
+      version: "latest",
+    },
+    {
+      name: "react-ui",
+      version: "latest",
+    },
+    {
+      name: "@mui/material",
+      version: "latest",
+    },
+    {
+      name: "@mui/styles",
+      version: "latest",
+    },
+    {
+      name: "@emotion/react",
+      version: "latest",
+    },
+    {
+      name: "@emotion/styled",
+      version: "latest",
+    },
+    {
+      name: "@mui/icons-material",
+      version: "latest",
+    },
+    {
+      name: "react-player",
+      version: "latest",
+    },
+  ];
+  
