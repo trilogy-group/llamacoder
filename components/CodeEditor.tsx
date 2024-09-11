902a8227e267c49d71003da1d6b7c27af6bcbd9b
amandal-cn <mandal.ashish@codenation.co.in>
2024-09-11 05:32:48 +0530

    fixed reset button issues

--- components/CodeEditor.tsx
+++ components/CodeEditor.tsx
@@ -17,8 +17,8 @@ import { saveAs } from "file-saver";
 import CircularProgress from "@mui/material/CircularProgress";
 import { Artifact } from "../types/Artifact";
 import { Project } from "../types/Project"; // Assuming you have a Project type
-import ErrorOutlineIcon from '@mui/icons-material/ErrorOutline';
-import RefreshIcon from '@mui/icons-material/Refresh';
+import ErrorOutlineIcon from "@mui/icons-material/ErrorOutline";
+import RefreshIcon from "@mui/icons-material/Refresh";
 
 interface CodeEditorProps {
   project: Project;
@@ -128,14 +128,12 @@ function SandpackContent({
 
   return (
     <div className="relative">
-      <div className="flex items-center gap-4 py-2">
-        {children}
-      </div>
+      <div className="flex items-center gap-4 py-2">{children}</div>
       <SandpackLayout>
         {!isPreviewOnly && (
           <SandpackCodeEditor
             style={{ height: "calc(80vh - 10px)", width: "50%" }}
-            showRunButton={true}
+            showRunButton={false}
             showInlineErrors={true}
             wrapContent={true}
             showLineNumbers={true}
@@ -153,13 +151,19 @@ function SandpackContent({
           <SandpackPreview
             style={{
               height: "calc(80vh - 10px)",
-              width: "100%"
+              width: "100%",
             }}
+            showRefreshButton={true}
+            showNavigator={false}
+            showRestartButton={true}
+            showOpenInCodeSandbox={true}
+            showOpenNewtab={false}
+            showSandpackErrorOverlay={false}
           />
           {statusMessage && (
             <div
               className="absolute inset-0 z-10 flex items-center justify-center"
-              style={{ backgroundColor: '#1e2029' }}
+              style={{ backgroundColor: "#1e2029" }}
             >
               <div className="flex flex-col items-center">
                 <CircularProgress size={60} thickness={4} color="primary" />
@@ -172,17 +176,19 @@ function SandpackContent({
         </div>
         {statusMessage === "" && actionButtons}
       </SandpackLayout>
-      <div className="absolute bottom-2 left-0 right-0 flex justify-center z-10">
-        <button
-          onClick={onReset}
-          className="sp-icon-standalone sp-c-bxeRRt sp-c-gMfcns sp-c-dEbKhQ sp-button flex items-center gap-2 border border-gray-300 rounded-full px-4 py-2"
-          title="Reset Sandbox"
-        >
-          <RefreshIcon style={{ width: "16px", height: "16px" }} />
-          <span>Reset</span>
-        </button>
-      </div>
-      {errorMessage && (
+      {statusMessage !== "" && (
+        <div className="absolute bottom-2 left-0 right-0 z-10 flex justify-center">
+          <button
+            onClick={onReset}
+            className="sp-icon-standalone sp-c-bxeRRt sp-c-gMfcns sp-c-dEbKhQ sp-button flex items-center gap-2 rounded-full border border-gray-300 px-4 py-2"
+            title="Reset Sandbox"
+          >
+            <RefreshIcon style={{ width: "16px", height: "16px" }} />
+            <span>Reset</span>
+          </button>
+        </div>
+      )}
+      {/* {errorMessage && (
         <button
           onClick={handleFixIt}
           className={`flex items-center gap-2 absolute top-6 right-2 z-50 font-medium py-2 px-4 rounded-full transition-colors
@@ -196,7 +202,7 @@ function SandpackContent({
           <ErrorOutlineIcon style={{ width: "16px", height: "16px" }} />
           <span>Auto Fix</span>
         </button>
-      )}
+      )} */}
     </div>
   );
 }
@@ -214,7 +220,7 @@ export default function CodeEditor({
       [];
     return allDependencies.reduce(
       (acc, dep) => {
-        acc[dep.name] =  "latest";
+        acc[dep.name] = "latest";
         return acc;
       },
       {} as Record<string, string>,
@@ -254,7 +260,7 @@ export default function CodeEditor({
 
   const handleReset = () => {
     console.log("handleReset: ", key);
-    setKey(prevKey => prevKey + 1);
+    setKey((prevKey) => prevKey + 1);
   };
 
   return (
