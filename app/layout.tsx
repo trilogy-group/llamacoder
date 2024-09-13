268c079dd1bf2396b3325829e35e544775610cc4
Rishabh Raizada <110235735+rishabh-ti@users.noreply.github.com>
2024-09-13 17:49:18 +0530

    Add Posthog (#19)

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,50 +1,52 @@
-import type { Metadata } from "next";
-import PlausibleProvider from "next-plausible";
-import "./globals.css";
-import { AppProvider } from "@/contexts/AppContext";
+import { AppProvider } from '@/contexts/AppContext'
+import { PHProvider } from '@/lib/posthog'
+import type { Metadata } from 'next'
+import dynamic from 'next/dynamic'
+import './globals.css'
 
-let title = "ArTIfacts – AI-Powered App Generator by TI";
-let description = "Generate your next app with ArTIfacts";
-let url = "https://github.com/trilogy-group/";
-let ogimage = "https://artifacts.ai/og-image.png";
-let sitename = "llamacoder.io";
+let title = 'ArTIfacts - AI-Powered App Generator by TI'
+let description = 'Generate your next app with ArTIfacts'
+let url = 'https://github.com/trilogy-group/'
+let ogimage = 'https://artifacts.ai/og-image.png'
+let sitename = 'llamacoder.io'
 
 export const metadata: Metadata = {
-  metadataBase: new URL(url),
-  title,
-  description,
-  icons: {
-    icon: "/favicon.ico",
-  },
-  openGraph: {
-    images: [ogimage],
-    title,
-    description,
-    url: url,
-    siteName: sitename,
-    locale: "en_US",
-    type: "website",
-  },
-  twitter: {
-    card: "summary_large_image",
-    images: [ogimage],
-    title,
-    description,
-  },
-};
+	metadataBase: new URL(url),
+	title,
+	description,
+	icons: {
+		icon: '/favicon.ico',
+	},
+	openGraph: {
+		images: [ogimage],
+		title,
+		description,
+		url: url,
+		siteName: sitename,
+		locale: 'en_US',
+		type: 'website',
+	},
+	twitter: {
+		card: 'summary_large_image',
+		images: [ogimage],
+		title,
+		description,
+	},
+}
 
-export default function RootLayout({
-  children,
-}: {
-  children: React.ReactNode
-}) {
-  return (
-    <html lang="en">
-      <body>
-        <AppProvider>
-          {children}
-        </AppProvider>
-      </body>
-    </html>
-  )
+const PostHogPageView = dynamic(() => import('../lib/posthog'), {
+	ssr: false,
+})
+
+export default function RootLayout({ children }: { children: React.ReactNode }) {
+	return (
+		<html lang="en">
+			<PHProvider>
+				<body>
+					<PostHogPageView />
+					<AppProvider>{children}</AppProvider>
+				</body>
+			</PHProvider>
+		</html>
+	)
 }
