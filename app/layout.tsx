e79f1af29536746c3a1905a60227c8d4fdcf3b1b
Abhishek Prusty <prusty.abhishek@codenation.co.in>
2024-09-18 22:19:14 +0530

    Cleanup

--- app/layout.tsx
+++ app/layout.tsx
@@ -4,36 +4,6 @@ import type { Metadata } from 'next'
 import dynamic from 'next/dynamic'
 import './globals.css'
 
-let title = 'ArTIfacts - AI-Powered App Generator by TI'
-let description = 'Generate your next app with ArTIfacts'
-let url = 'https://github.com/trilogy-group/'
-let ogimage = 'https://artifacts.ai/og-image.png'
-let sitename = 'llamacoder.io'
-
-export const metadata: Metadata = {
-	metadataBase: new URL(url),
-	title,
-	description,
-	icons: {
-		icon: '/favicon.ico',
-	},
-	openGraph: {
-		images: [ogimage],
-		title,
-		description,
-		url: url,
-		siteName: sitename,
-		locale: 'en_US',
-		type: 'website',
-	},
-	twitter: {
-		card: 'summary_large_image',
-		images: [ogimage],
-		title,
-		description,
-	},
-}
-
 const PostHogPageView = dynamic(() => import('../lib/posthog'), {
 	ssr: false,
 })
